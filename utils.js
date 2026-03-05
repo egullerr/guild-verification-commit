98b0ac567cc298a429326918f8dda4614bfd6ec6
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 22:50:38 +0300

    Correct typo in configuration file

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:50:20.030Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T19:50:38.326Z
+Commit: Correct typo in configuration file
