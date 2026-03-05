192e253dcb4d66a9dd1732fef746843f60883aa5
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 22:50:00 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:49:51.121Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T19:50:00.277Z
+Commit: Resolve timezone issues in date handling
