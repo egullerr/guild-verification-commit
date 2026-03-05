56a75593dba485e7218d4dc1c536d0ba8ae73e9e
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 22:50:33 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:50:00.277Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:50:33.035Z
+Commit: Fix broken links in documentation
