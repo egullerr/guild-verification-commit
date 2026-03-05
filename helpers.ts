87030bb3f785f0e61c5f06854c4c2d767363a412
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 22:49:18 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:58:04.308Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T19:49:18.339Z
+Commit: Patch API endpoint error handling
