80d03eecae5f10370a80f7ca5b2620d904427196
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 20:56:01 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:50:29.138Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:56:00.979Z
+Commit: Patch API endpoint error handling
