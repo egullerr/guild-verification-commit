1e52a208809af72bd277ebaf91179193447c26e9
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 20:58:04 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:57:59.920Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:58:04.308Z
+Commit: Fix CSS styling bug on mobile devices
