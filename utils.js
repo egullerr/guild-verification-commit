24431606606ea8eeaf20bb40ae2a69feb9c283a3
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 22:50:40 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:50:38.326Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T19:50:39.578Z
+Commit: Resolve timezone issues in date handling
