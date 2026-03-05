70aaada6a67154862a2b9b96f2e61ee4a699e201
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 20:57:54 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:56:00.979Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T17:57:53.895Z
+Commit: Fix broken links in documentation
