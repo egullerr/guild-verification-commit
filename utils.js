88e1cf46ad06934aa086ecb71f400e75357eb4eb
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 20:58:11 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:58:05.704Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T17:58:10.820Z
+Commit: Patch API endpoint error handling
