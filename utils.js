d9f665a86e11a37dc1aa617adf1ea6ad4f50a8ff
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-05 22:50:11 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:49:38.071Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:50:10.724Z
+Commit: Fix CSS styling bug on mobile devices
