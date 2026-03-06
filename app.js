3d850273c2cde677d9357cdca64f82fdb20c1aae
egullerr <71702809+egullerr@users.noreply.github.com>
2026-03-06 04:35:51 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T01:26:49.433Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T01:35:50.849Z
+Commit: Fix broken links in documentation
